d8a86a38b96daa5fb8fb6fdc29f7f0046eac5c94
Shoohei <rouillardtonyp@gmail.com>
2020-09-22 19:44:00 +0900

    4 files modified, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,32 @@ import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgJsonEditorModule } from 'ang-jsoneditor';
 
 import { AppComponent } from "./app.component";
 import { ConfigServerService } from "./config-server.service";
 import { InterfacesListComponent } from "./interfaces-list/interfaces-list.component";
-import { InterfaceComponent } from './interface/interface.component';
-import { ConfigFieldComponent } from './config-field/config-field.component';
-import { NgJsonEditorModule } from 'ang-jsoneditor';
-
+import { InterfaceComponent } from "./interface/interface.component";
+import { ConfigFileComponent } from "./config-file/config-file.component";
 
 @NgModule({
-  imports: [BrowserModule, HttpClientModule, FormsModule, NgJsonEditorModule,
-  RouterModule.forRoot([
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    NgJsonEditorModule,
+    RouterModule.forRoot([
       { path: "", component: InterfacesListComponent },
       { path: "interface/:interface", component: InterfaceComponent }
-    ])],
-  declarations: [AppComponent, InterfacesListComponent, InterfaceComponent, ConfigFileComponent, ConfigFieldComponent],
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    InterfacesListComponent,
+    InterfaceComponent,
+    ConfigFileComponent
+  ],
   bootstrap: [AppComponent],
   providers: [ConfigServerService]
 })
